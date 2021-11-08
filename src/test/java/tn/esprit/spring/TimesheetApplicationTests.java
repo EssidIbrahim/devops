48c638702559f1a3f07f22c4dee3e21f0d6c0277
Essid-dev <ibrahim.essid@neovia.co>
2021-11-08 12:47:49 +0100

    devops jenkins

---
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
